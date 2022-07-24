2c053bb1b885fa547ae78aa5d7480d3e61296fa8
sivchari <shibuuuu5@gmail.com>
2022-07-24 23:21:47 +0900

    feat: simple client

--- libs/chronicle-client/src/lib.rs
+++ libs/chronicle-client/src/lib.rs
@@ -1,5 +1,6 @@
 use crate::url::Url;
 use chronicle_common::request::{Request, RequestMethod};
+use chronicle_common::response::Response;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str;
@@ -16,14 +17,15 @@ impl Client {
     pub fn get(url: String) {
         let url = Url::new(&url);
 
-        let request = Request::build(url.pathname, RequestMethod::GET);
+        let request = Request::build(url.pathname, RequestMethod::GET, url.host);
+        let host = request.host.clone();
         let request = request.print();
-        let mut stream = Client::connect(url.host);
-        stream.write(request.as_bytes());
-        let mut buffer = [0; 2048];
-        let nbytes = stream.read(&mut buffer).unwrap();
-        let http = str::from_utf8(&buffer).unwrap();
-        println!("{}", http);
+        let mut stream = Client::connect(host);
+        let request = request.as_bytes();
+        stream.write(request);
+        stream.write(&[0]);
+        let response = Response::parse_stream_to_response(&mut stream);
+        response.print();
     }
 
     fn connect(host: String) -> TcpStream {
